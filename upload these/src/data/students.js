848f7c403f34965011cd18fed5d207e30c8c0193
Swanand <swanandjaju@gmail.com>
2026-06-05 15:23:30 +0530

    Remove redundant 'upload these' folder from Git tracking

--- upload these/src/data/students.js
+++
@@ -1,9 +0,0 @@
-export const DEMO_STUDENTS = [
-  { id: 'STU001', name: 'Yash', rollNumber: '2GI25CV108' },
-  { id: 'STU002', name: 'Omkar', rollNumber: '2GI25ME056' },
-  { id: 'STU003', name: 'Bhavana', rollNumber: '2GI25CS045' },
-  { id: 'STU004', name: 'Kartik', rollNumber: '2GI25EC056' },
-];
-
-export const GEOFENCE_RADIUS_METERS = 20;
-export const MONITORING_DURATION_MS = 120000;
